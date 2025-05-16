00bec8b95403834bde8949881980711c1525f2df
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-05-17 00:31:21 +0200

    Implement the plot drawing

---
+++ ThreeBodySimulationPlotDrawer/PlotDrawer.fs
@@ -0,0 +1,32 @@
+﻿module PlotDrawer
+
+open ThreeBodySimulation.Simulation
+open Plotly.NET
+
+type SimPlotOptions = {
+    drawCenterOfMass : bool
+}
+
+let defaultSimPlotOptions = {
+    drawCenterOfMass = false
+}
+
+let plotSim (simulator : BodiesSimulator) startTime endTime (options : SimPlotOptions) = 
+    let body1Positions = ResizeArray()
+    let body2Positions = ResizeArray()
+    let body3Positions = ResizeArray()
+
+    for state in simulator.Simulate(startTime, endTime) do
+        body1Positions.Add (state.Body1Position.X, state.Body1Position.Y, state.Body1Position.Z)
+        body2Positions.Add (state.Body2Position.X, state.Body2Position.Y, state.Body2Position.Z)
+        body3Positions.Add (state.Body3Position.X, state.Body3Position.Y, state.Body3Position.Z)
+
+    Chart.combine [
+        // TODO: implement center of mass drawing
+        if options.drawCenterOfMass then 
+            yield Chart.Line3D(xyz = [], Name="Center of Mass")
+        
+        yield Chart.Line3D(xyz = body1Positions, Name="Body 1")
+        yield Chart.Line3D(xyz = body2Positions, Name="Body 2")
+        yield Chart.Line3D(xyz = body3Positions, Name="Body 3")
+    ]
--- ThreeBodySimulationPlotDrawer/Program.fs
+++ ThreeBodySimulationPlotDrawer/Program.fs
@@ -1,3 +1,38 @@
-﻿open Plotly.NET
+﻿open PlotDrawer
+open Plotly.NET
 
+open ThreeBodySimulation.Data
+open ThreeBodySimulation.Simulation
+open ThreeBodySimulation.Simulation.Solvers
 
+let G = 1
+
+let body1Pos = BodyPosition(4.5, 5, -3)
+let body1Vel = BodyPosition(0, 0, 0)
+let body1Mass = 10
+
+let body2Pos = BodyPosition(-3, 0, 3)
+let body2Vel = BodyPosition(0, 0, 0)
+let body2Mass = 20
+
+let body3Pos = BodyPosition(-6, -5, -5)
+let body3Vel = BodyPosition(0, 0, 0)
+let body3Mass = 30
+
+let startTime = 0
+let endTime = 100
+
+let step = 0.0001
+
+let body1 = Body(body1Pos, body1Vel, body1Mass)
+let body2 = Body(body2Pos, body2Vel, body2Mass)
+let body3 = Body(body3Pos, body3Vel, body3Mass)
+
+let solver = Yoshida4BodiesSolver() 
+solver.Step <- step
+
+let simulator = BodiesSimulator(body1, body2, body3, solver, G)
+
+let chart = plotSim simulator startTime endTime defaultSimPlotOptions
+
+chart |> Chart.show
