1335ba30091567c49c7307ca3347039a2b92d60b
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-05-17 01:11:55 +0200

    Add benchmarks for the solvers

---
+++ ThreeBodySimulation.Benchmarks/ThreeBodySimulatorProvider.cs
@@ -0,0 +1,23 @@
+﻿using ThreeBodySimulation.Data;
+using ThreeBodySimulation.Simulation;
+using ThreeBodySimulation.Simulation.Solvers;
+
+namespace ThreeBodySimulation.Benchmarks;
+
+public static class ThreeBodySimulatorProvider
+{
+    /// <summary>
+    /// Provides a test simulator with pre-defined values for the given 
+    /// <paramref name="solver"/>.
+    /// </summary>
+    /// <param name="solver">Solver to use.</param>
+    /// <returns>A test simulator with pre-defined values.</returns>
+    public static BodiesSimulator GetSimulator(IBodiesSolver solver)
+    {
+        Body body1 = new(new(4.5, 0, -3), new(2, 0, 0), 10);
+        Body body2 = new(new(-3, 0, 3), new(0, 0, 0), 20);
+        Body body3 = new(new(-6, -5, -5), new(0, 0, 0), 30);
+
+        return new(body1, body2, body3, solver, 1);
+    }
+}
---
+++ ThreeBodySimulation.Benchmarks/ThreeBodySolversBenchmarks.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using ThreeBodySimulation.Simulation.Solvers;
+
+namespace ThreeBodySimulation.Benchmarks;
+
+public class ThreeBodySolversBenchmarks
+{
+    private readonly Consumer _consumer = new Consumer();
+
+    [Params(1e-3, 1e-4, 1e-5)]
+    public double Step { get; set; }
+
+    [Params(1)]
+    public double Time { get; set; }
+
+    private void Solve(IBodiesSolver solver)
+    {
+        var simulator = ThreeBodySimulatorProvider.GetSimulator(solver);
+        var states = simulator.Simulate(startTime: 0.0, endTime: Time);
+
+        foreach (var state in states)
+            _consumer.Consume(state);
+    }
+
+    [Benchmark]
+    public void Yoshida4()
+    {
+        Yoshida4BodiesSolver solver = new() { Step = Step };
+        Solve(solver);
+    }
+
+    [Benchmark]
+    public void RK4()
+    {
+        RK4BodiesSolver solver = new() { Step = Step };
+        Solve(solver);
+    }
+}
