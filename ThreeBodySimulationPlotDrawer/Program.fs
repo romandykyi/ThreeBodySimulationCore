a1053cda9368b57bccb73856d5c314b48803a4ea
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-05-17 14:40:52 +0200

    Update the initial state

--- ThreeBodySimulationPlotDrawer/Program.fs
+++ ThreeBodySimulationPlotDrawer/Program.fs
@@ -7,22 +7,22 @@ open ThreeBodySimulation.Simulation.Solvers
 
 let G = 1
 
-let body1Pos = BodyPosition(4.5, 5, -3)
-let body1Vel = BodyPosition(0, 0, 0)
+let body1Pos = BodyPosition(0.9700436, -0.24308753, 0)
+let body1Vel = BodyPosition(0.466203685, 0.43236573, 2)
 let body1Mass = 10
 
-let body2Pos = BodyPosition(-3, 0, 3)
-let body2Vel = BodyPosition(0, 0, 0)
-let body2Mass = 20
+let body2Pos = BodyPosition(-0.9700436, 0.24308753, 0)
+let body2Vel = BodyPosition(0.466203685, 0.43236573, 0)
+let body2Mass = 10
 
-let body3Pos = BodyPosition(-6, -5, -5)
-let body3Vel = BodyPosition(0, 0, 0)
-let body3Mass = 30
+let body3Pos = BodyPosition(0, 0, 0)
+let body3Vel = BodyPosition(-0.93240737, -0.86473146, 0)
+let body3Mass = 10
 
 let startTime = 0
-let endTime = 100
+let endTime = 1.5
 
-let step = 0.0001
+let step = 0.00001
 
 let body1 = Body(body1Pos, body1Vel, body1Mass)
 let body2 = Body(body2Pos, body2Vel, body2Mass)
