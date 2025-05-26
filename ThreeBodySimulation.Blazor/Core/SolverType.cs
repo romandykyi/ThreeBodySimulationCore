50673ecdbd985bdeb37c028205d4f9281a087129
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-05-27 01:02:33 +0200

    Add better solver display names

--- ThreeBodySimulation.Blazor/Core/SolverType.cs
+++ ThreeBodySimulation.Blazor/Core/SolverType.cs
@@ -1,7 +1,11 @@
-﻿namespace ThreeBodySimulation.Blazor.Core;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThreeBodySimulation.Blazor.Core;
 
 public enum SolverType
 {
+    [Display(Name = "Runge-Kutta (4th order)")]
     RK4,
+    [Display(Name = "Yoshida (4th order)")]
     Yoshida4
 }
